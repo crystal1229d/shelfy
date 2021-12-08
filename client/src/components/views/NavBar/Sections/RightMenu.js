2bbd0bcc3811fd8d49422ae79cdb3cef8202aa2a
crystal1229d <crystal1229d@naver.com>
2021-12-08 23:15:37 +0900

    (1)Install antd  (2)Add BookUpload Component to navBar

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -36,6 +36,9 @@ function RightMenu(props) {
         <Menu.Item key="logout">
           <a onClick={logoutHandler}>Logout</a>
         </Menu.Item>
+        <Menu.Item key="upload">
+          <a href="/book/upload">Upload</a>
+        </Menu.Item>
       </Menu>
     )
   }
---
+++ client/src/components/views/UploadBookPage/UploadBookPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UploadBookPage() {
+    return (
+        <div>
+            UploadBookPage
+        </div>
+    )
+}
+
+export default UploadBookPage
