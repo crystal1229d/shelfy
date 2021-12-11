ac365511ffcae0a41b0f7b5156d06df94e6b46c1
crystal1229d <crystal1229d@naver.com>
2021-12-11 23:34:47 +0900

    add UploadBookPage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -22,7 +22,7 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
-          <Route exact path="/book/upload" component={Auth(UploadBookPage, false)} />
+          <Route exact path="/book/upload" component={Auth(UploadBookPage, true)} />
         </Switch>
       </div>
       <Footer />
--- client/src/components/views/UploadBookPage/UploadBookPage.js
+++ client/src/components/views/UploadBookPage/UploadBookPage.js
@@ -1,9 +1,48 @@
 import React from 'react'
+import { Typography, Button, Form, Input, Rate, Select } from 'antd';
+
+const { Title: TitleTag } = Typography;
+const { Option } = Select;
+const { TextArea } = Input;
 
 function UploadBookPage() {
+
+    const SubmitHandler = event => {
+        console.log('SubmitHandler')
+    }
+
     return (
         <div>
-            UploadBookPage
+            <div><TitleTag level={2}>책 정보 업로드</TitleTag></div>
+            <div>
+                <Form
+                    onFinish={SubmitHandler} 
+                    initialValues={{
+                        rate: 5.0
+                    }}
+                >
+
+                    <Form.Item
+                        name="select-multiple"
+                        label="Select[multiple]"
+                        rules={[ {
+                            required: true,
+                            message: 'Please select genre of the book!',
+                            type: 'array',
+                        }, ]}>
+                        <Select mode="multiple" placeholder="Please select genre">
+                            <Option value="fiction">Fiction</Option>
+                            <Option value="non-fiction">Non-Fiction</Option>
+                            <Option value="sf">SF</Option>
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item name="rate" label="Rate">
+                        <Rate />
+                    </Form.Item>
+
+                </Form>
+            </div>
         </div>
     )
 }
