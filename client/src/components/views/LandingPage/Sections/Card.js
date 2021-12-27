1d4bf14ed0c4b22676168171a8e5680ffd9f4f24
crystal1229d <crystal1229d@naver.com>
2021-12-27 22:31:53 +0900

    fix Card

--- client/src/components/views/LandingPage/Sections/Card.js
+++ client/src/components/views/LandingPage/Sections/Card.js
@@ -1,15 +1,26 @@
 import React from 'react'
 
 function Card(props) {
+
+    const renderAuthors = () => {
+        return props.book.author[0];
+    }
+
+    const renderGenres = () => {
+        props.book && props.book.genre.map((item) => (
+            <div>{item}</div>
+        ));
+    }
+
     return (
         <div style={{ width:'270px', height:'290px', display:'grid', gridTemplateRows:'200px auto' }}>
             <div style={{ display:'flex', justifyContent:'center', alignItems:'center', background:'lightgray', borderRadius:'7px' }}>
                 image
             </div>
-            <div style={{ display:'grid', }}>
-                <div>{props.book.title}</div>
-                <div>{props.book.author[0]}</div>
-                <div>{props.book.genre[0]}</div>
+            <div style={{ display:'grid', padding:'0 4px' }}>
+                <div style={{ fontWeight:'800', color:'blueviolet' }}>{props.book.title}</div>
+                <div>{renderAuthors()}</div>
+                <div>{renderGenres()}</div>
                 <div>{props.book.plot}</div>
             </div>
         </div>
